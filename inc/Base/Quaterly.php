dad251923b76265ac3cf52908e3eadf0abeccfef
Rodrigo Garbulha <rg@acmedent.local>
2020-01-02 11:57:29 -0500

    Quaterly Pdf

--- inc/Base/Quaterly.php
+++ inc/Base/Quaterly.php
@@ -41,66 +41,66 @@ class Quaterly
                 "link"=>$pdfPath."Dentsply_Q1_2020.pdf",
                 "alt" => "Dentsply"
             )
-            ),
+            ,
             array(
                 "src" => "/wp-content/uploads/2019/06/diadent.png",
                 "link"=>$pdfPath."DiaDent_2020_Q1.pdf",
                 "alt" => "Diadent"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/dmg-1.png",
                 "link"=>$pdfPath."DMG_Q1_2020.pdf",
                 "alt" => "DMG"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/gc-america.png",
                 "link"=>$pdfPath."GCAmerica_Q1-2020.pdf",
                 "alt" => "GC"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/morita.png",
                 "link"=>$pdfPath."J.Morita_Q1_2020.pdf",
                 "alt" => "JMorita"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/kulzer.jpg",
                 "link"=>$pdfPath."Kulzer_Q1_2020.pdf",
                 "alt" => "Kulzer"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/kuraray.png",
                 "link"=>$pdfPath."Kuraray-2020-Q1-PANAVIA-sm.pdf",
                 "alt" => "Kuraray Panavia"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/kuraray.png",
                 "link"=>$pdfPath."Kuraray_2020-Q1-CLEARFIL-sm.pdf",
                 "alt" => "Kuraray Clearfil"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/medicom.png",
                 "link"=>$pdfPath."Medicom_Q1_2020.pdf",
                 "alt" => "Medicom"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/puldent.png",
                 "link"=>$pdfPath."Pulpdent-1st-Quarter-2020.pdf",
                 "alt" => "Pulpdent"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2019/06/sable-1-e1577983965932.png",
                 "link"=>$pdfPath."Sable_2020_Q1.pdf",
                 "alt" => "Sable"
-            )
+            
             ),
             array(
                 "src" => "/wp-content/uploads/2020/01/Waterpik-Logo.png",
