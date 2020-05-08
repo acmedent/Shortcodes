e277d5963041c689a4bf7ca0cd9daf09cdd9c479
Rodrigo Garbulha <rgarbulha@acmedent.com>
2020-05-08 14:02:25 -0400

    weekly sale

--- inc/Base/Home.php
+++ inc/Base/Home.php
@@ -23,7 +23,7 @@ class Home
 
 
         //Date that start next weeks promo
-        $start_date = '2020-05-04';
+        $start_date = '2020-05-11';
         $date_from_user = date('Y-m-d');
 
         if ($this->check_in_range($start_date, $date_from_user))
@@ -151,15 +151,15 @@ class Home
         
         <img src='/wp-content/uploads/2020/03/SpringSale.jpg' alt='WEEKLY FLASH SALE!' class='promo-logo'/>
 
-        <h2 class='promo-date'>April 27 - May 03, 2020</h2>";
+        <h2 class='promo-date'>May 04 - 10, 2020</h2>";
 
         $ids = array(
-            43189 => array("promo" => "10% OFF", "promo-price" => "18.63", "net-price" => ""),
             51011 => array("promo" => "", "promo-price" => "39.95", "net-price" => ""),
-            26321 => array("promo" => "10% OFF", "promo-price" => "49.82", "net-price" => ""),
-            42712 => array("promo" => "5% OFF", "promo-price" => "62.42", "net-price" => ""),
-            26843 => array("promo" => "5+1 | 15+6", "promo-price" => "118.42", "net-price" => ""),
-            38601 => array("promo" => "10% OFF", "promo-price" => "50.22", "net-price" => ""),
+            51051 => array("promo" => "New!", "promo-price" => "36.95", "net-price" => ""),
+            33534 => array("promo" => "15% OFF", "promo-price" => "41.64", "net-price" => ""),
+            26847 => array("promo" => "10% OFF", "promo-price" => "36.86", "net-price" => ""),
+            27827 => array("promo" => "15% OFF", "promo-price" => "14.15", "net-price" => ""),
+            32454 => array("promo" => "10% OFF", "promo-price" => "284.72", "net-price" => ""),
         );
 
         foreach ($ids as $id => $value) {
@@ -220,15 +220,13 @@ class Home
 
         <img src='/wp-content/uploads/2020/03/SpringSale.jpg' alt='WEEKLY FLASH SALE!' class='promo-logo'/>
 
-        <h2 class='promo-date'>May 04 - 10, 2020</h2>";
+        <h2 class='promo-date'>Until Quantities Last</h2>";
 
         $ids = array(
             51011 => array("promo" => "", "promo-price" => "39.95", "net-price" => ""),
-            51051 => array("promo" => "New!", "promo-price" => "36.95", "net-price" => ""),
-            33534 => array("promo" => "15% OFF", "promo-price" => "41.64", "net-price" => ""),
-            26847 => array("promo" => "10% OFF", "promo-price" => "36.86", "net-price" => ""),
-            27827 => array("promo" => "15% OFF", "promo-price" => "14.15", "net-price" => ""),
-            32454 => array("promo" => "10% OFF", "promo-price" => "284.72", "net-price" => ""),
+            51051 => array("promo" => "", "promo-price" => "36.95", "net-price" => ""),
+            51102 => array("promo" => "", "promo-price" => "149.95", "net-price" => ""),
+            51111 => array("promo" => "", "promo-price" => "19.95", "net-price" => ""),
         );
 
         foreach ($ids as $id => $value) {
--- inc/Base/Terms.php
+++ inc/Base/Terms.php
@@ -5,6 +5,7 @@
  */
 
 namespace Inc\Base;
+
 if (!defined('ABSPATH')) {
     die;
 };
@@ -14,6 +15,7 @@ class Terms
     function __construct()
     {
         add_shortcode('acme-terms', array($this, 'AcmedentTermsPage'));
+        add_shortcode('acme-carousel', array($this, 'AcmedentCarousel'));
     }
 
 
@@ -127,6 +129,38 @@ class Terms
     </div>";
 
 
+
+        return $page;
+    }
+
+    function AcmedentCarousel()
+    {
+        $page = '
+        <style>
+        .label-big{
+            font-size:1.5rem;
+        }
+        </style>
+
+        <div class="container mt-5">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We will never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1" class="label-big" >Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label ml-5" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary my-3">Submit</button>
+        </form>
+        </div>
+        ';
 
         return $page;
     }
