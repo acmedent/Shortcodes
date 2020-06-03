2c90cfe7697fe7026341b7f5af792c9ade4772a2
Rodrigo Garbulha <rgarbulha@acmedent.com>
2020-06-03 12:27:43 -0400

    mailchimp api

--- inc/Base/HomePage.php
+++ inc/Base/HomePage.php
@@ -15,6 +15,7 @@ class HomePage
     function __construct()
     {
         add_shortcode('home-slider', array($this, 'HomeSlider'));
+        add_shortcode('home-showcase', array($this, 'HomeShowCase'));
     }
 
 
@@ -26,6 +27,15 @@ class HomePage
 
         // $template .= do_shortcode('[products limit="6" columns="6" best_selling="true" ]');
         // $template .= do_shortcode('[product sku="EN4262" ]');
+
+        return $template;
+    }
+
+    function HomeShowCase()
+    {
+        $path = home_url() . "/wp-content/plugins/Shortcodes/inc/Base/templates/";
+        $template = file_get_contents($path . 'showcase.tpl.html', FILE_USE_INCLUDE_PATH);
+
 
         return $template;
     }
--- inc/Base/Pages.php
+++ inc/Base/Pages.php
@@ -38,6 +38,30 @@ class Pages
             },500);
             </script>";
 
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://us4.api.mailchimp.com/3.0/lists/2555c280d2",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => "{\n\t\"members\":[\n\t\t{\n\t\t\"email_address\":\"" . $_POST['email_campaign'] . "\",\n\t\t\"status\":\"subscribed\",\n\t\t\"tags\":[\n\t\t\t\"General\"\n\t\t\t]\n\t\t}\n\t]\n}",
+                CURLOPT_HTTPHEADER => array(
+                    "Content-Type: application/json",
+                    "Authorization: auth c7d3c2ed44491f1836fdda9859be9f0a-us4",
+                    "Content-Type: text/plain",
+                    "Cookie: ak_bmsc=F8C513D2CD4F61EB63EB9A7C701B74F248F62B7D1B6E00009AB5D75ED335CB15~plZGHaG429QIeg/QUnYNDTU+b3Nvf5l2W+OzWpjEABzddU3LwZ1PuZd8kVZKUkGK3C/td0cL1QEe9ss8vc+DHDQWODj3NFC/z0z1svecHdoLALUHcqEIaNUuIwf4ZoZwiUxGX63nCVNSwmkoRxyUGu81LrSLCvV2qteTLD4c5cTxhEnVE9FstGsMDPCHPM7CqY0Hdi38BVLFCT70nPNapojLaR92Zd7mLOjVSirrkztcI="
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
             $to = "rgarbulha@acmedent.com";
             $subject = "Subscribed email.";
             $txt = $_POST['email_campaign'];
